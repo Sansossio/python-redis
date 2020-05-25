eebf4ab7d6cfc0e26aa024a7503212cf47c36d06
Sansossio <juliosansossio@gmail.com>
2020-05-25 07:17:39 +0200

    update quotes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import redis
 import os
 
-host = os.getenv('REDIS_HOST') or 'localhost'
-port = os.getenv('REDIS_PORT') or 6379
+host = os.getenv("REDIS_HOST") or "localhost"
+port = os.getenv("REDIS_PORT") or 6379
 r = redis.Redis(host, port)
 
-r.set('test', 'value')
+r.set("test", "value")
 
-print(r.get('test'))
+print(r.get("test"))
 # Print = b"value"
